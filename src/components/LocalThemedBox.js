18ee5de92ee1f1ef1700bd34fe421598785570b5
Vivek Kumar <95522287+GitsOfVivek@users.noreply.github.com>
2023-03-05 14:27:07 +0530

    Update LocalThemedBox.js

--- src/components/LocalThemedBox.js
+++ src/components/LocalThemedBox.js
@@ -1,14 +1,27 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 
 const LocalThemedBox = () => {
+	const [theme, setTheme] = useState('light');
+	return (
+		<div
+			style={{
+				width: '200px',
+				height: '200px',
+				border: '2px solid green',
+			}}
+			id="local-themed-box"
+			className={`txt-${theme} bg-${theme}`}>
+			<button
+				className={'btn ' + `btn-${theme}`}
+				id="themed-button"
+				onClick={() => {
+					const val = theme === 'light' ? 'dark' : 'light';
+					setTheme(val);
+				}}>
+				Switch to dark theme
+			</button>
+		</div>
+	);
+};
 
-return(
-    <div style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box">
-        {/* Write code below this line */}
-        
-    </div>
-)
-}
-
-export { LocalThemedBox }
+export { LocalThemedBox };
